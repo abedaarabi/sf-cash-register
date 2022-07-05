fe62a083479dbf3f20b04c0d9929b15272626455
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2022-07-05 16:20:18 +0200

    add emails as admin

--- helper/emailAdmin.ts
+++ helper/emailAdmin.ts
@@ -2,5 +2,7 @@ export const admin = [
   "abbasmroueh@gmail.com",
   "abedaarabi@gmail.com",
   "alaasfarhat@gmail.com",
+  "pedramak80@gmail.com",
+  "homaydan@hotmail.com",
   // "abma@moe.dk",
 ];
