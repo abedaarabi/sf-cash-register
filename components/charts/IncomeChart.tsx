ddebbe1fad0f173325463a768ba031a9753f69f0
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2022-07-21 21:37:59 +0200

    add loader

--- components/charts/IncomeChart.tsx
+++ components/charts/IncomeChart.tsx
@@ -15,6 +15,7 @@ const IncomeChart = () => {
     if (!date.startDate || !date.endDate) {
       return alert("select dates");
     } else {
+      setLoading(true);
       const startDate = date.startDate;
       const endDate = date.endDate;
 
@@ -46,6 +47,7 @@ const IncomeChart = () => {
     }
   }
 
+  React.useEffect(() => {}, [loading]);
   React.useEffect(() => {
     fetch(`/api/dailyreports/report`)
       .then((res) => res.json())
@@ -70,60 +72,57 @@ const IncomeChart = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  if (loading) {
-    return (
-      <div>
-        <h2 className={styles.container}>
-          <CircularProgress />
-        </h2>
-      </div>
-    );
-  }
-
   return (
     <div className={styles.charts}>
       <DateSelector getdate={getdate} />
-      <div className={styles.charts}>
-        <h2 style={{ color: "#6d6875" }}>Total: {+dailyReport.total} kr</h2>
-        <p style={{ color: "#6d6875" }}>
-          From: {dailyReport.label[0]} To:
-          {dailyReport.label[dailyReport.label.length - 1]}
-        </p>
-      </div>
 
-      <Line
-        height={window.innerWidth < 450 ? 85 : 35}
-        width={window.innerWidth < 450 ? 100 : 120}
-        datasetIdKey="id"
-        data={{
-          labels: dailyReport.label,
-          datasets: [
-            {
-              label: "Income",
-              data: dailyReport.data,
-              //@ts-ignore
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <div className={styles.charts}>
+            <h2 style={{ color: "#6d6875" }}>Total: {+dailyReport.total} kr</h2>
+            <p style={{ color: "#6d6875" }}>
+              From: {dailyReport.label[0]} To:
+              {dailyReport.label[dailyReport.label.length - 1]}
+            </p>
+          </div>
+
+          <Line
+            height={window.innerWidth < 450 ? 85 : 35}
+            width={window.innerWidth < 450 ? 100 : 120}
+            datasetIdKey="id"
+            data={{
+              labels: dailyReport.label,
+              datasets: [
+                {
+                  label: "Income",
+                  data: dailyReport.data,
+                  //@ts-ignore
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                  ],
+                  //@ts-ignore
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                  ],
+                  borderWidth: 3,
+                },
               ],
-              //@ts-ignore
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 3,
-            },
-          ],
-        }}
-      />
+            }}
+          />
+        </>
+      )}
     </div>
   );
 };
