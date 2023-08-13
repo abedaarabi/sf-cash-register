f9786fd7e4e90bf88ee48900e8d993dcfa4b91a3
Abdulrahman Mohamad Aarabi <abma@arteliagroup.dk>
2023-08-13 09:40:34 +0200

    [FIX] typo

--- helper/emailAdmin.ts
+++ helper/emailAdmin.ts
@@ -4,7 +4,7 @@ export const admin = [
   "alaasfarhat@gmail.com",
   "pedramak80@gmail.com",
   "homaydan@hotmail.com",
-  "Andrikopoulos1987@gmail.com",
+  "andrikopoulos1987@gmail.com",
   // "marcus.ja.jakobsen@gmail.com",
   // "abma@moe.dk",
 ];
